7d43727d28ca1fb6bbd2375aa752f57eaa6c40c1
Andrew Armenante <andrew.armenante@gmail.com>
2023-12-08 14:27:31 -0500

    Add secrets.yaml file for production deployment

---
+++ tools/load_secrets.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Define the name of your Kubernetes secret and the namespace (if needed)
+SECRET_NAME="prod-secrets"
+NAMESPACE="default"  # Change as needed
+
+# Specify the output file
+OUTPUT_FILE="deployment/prod/common/secrets.yaml"  # Change this to your desired file name
+
+# Start of the Secret YAML, redirecting output to the specified file
+echo "apiVersion: v1" > "$OUTPUT_FILE"
+echo "kind: Secret" >> "$OUTPUT_FILE"
+echo "metadata:" >> "$OUTPUT_FILE"
+echo "  name: $SECRET_NAME" >> "$OUTPUT_FILE"
+echo "  namespace: $NAMESPACE" >> "$OUTPUT_FILE"
+echo "type: Opaque" >> "$OUTPUT_FILE"
+echo "data:" >> "$OUTPUT_FILE"
+
+# Read each line in the .env file and add it to the secret
+while read -r line; do
+  if [[ ! -z "$line" ]]; then
+    KEY=$(echo $line | cut -f1 -d'=')
+    VALUE=$(echo $line | cut -f2- -d'=' | base64)
+    echo "  $KEY: $VALUE" >> "$OUTPUT_FILE"
+  fi
+done < .env
