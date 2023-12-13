7a182adf2deae239b9e3be6958edf986b13aad19
Andrew Armenante <andrew.armenante@gmail.com>
2023-12-13 21:54:51 +0000

    Refactor worker setup and add Redis deployment

--- apps/watcher/src/index.ts
+++ apps/watcher/src/index.ts
@@ -1,34 +1,20 @@
-import { SQSEvent } from 'aws-lambda';
-import { DevWorker, Types, Worker } from 'diner-utilities';
+import { DevWorker } from 'diner-utilities';
 import { runWorker } from './worker.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const handler = async (event: SQSEvent): Promise<void> => {
-    const { receiptHandle, body } = event.Records[0] as unknown as { receiptHandle: string, body: string };
-    const watcherConfig = JSON.parse(body) as Types.Watcher;
-
-    const worker = new Worker();
-
-    await runWorker(watcherConfig, receiptHandle, worker);
-};
-
 // Set up dev environment
-
-if (process.env.NODE_ENV === 'development') {  
-    console.log('Running dev worker'); 
-    const worker = new DevWorker();
+console.log('Running Worker'); 
+const worker = new DevWorker();
     
-    worker.queue.process(async (job) => {
-        console.log('DEV watcher job running');
-        console.log('DEV watcher job data');
-        await runWorker(job.data.message, job.data.id, worker);
-    });
+worker.queue.process(async (job) => {
+    console.log('Watcher job running');
+    await runWorker(job.data.message, job.data.id, worker);
+});
     
 
-    worker.queue.on('failed', async (job) => {
-        console.log('DEV watcher job failed');
-        console.log(job);
-    });
-}
+worker.queue.on('failed', async (job) => {
+    console.log('Watcher job failed');
+    console.log(job);
+});
